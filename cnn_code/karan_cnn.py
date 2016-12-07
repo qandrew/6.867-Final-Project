d1d0f4120b1bb2c945ea6cc7b49e025ccdb0293c
Andrew Xia <axia@mit.edu>
2016-12-07 17:21:54 -0500

    add plot feature to cnn

--- cnn_code/karan_cnn.py
+++ cnn_code/karan_cnn.py
@@ -7,9 +7,14 @@ from load_data import get_data as ld
 import tensorflow as tf
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 testdir = '/Users/karan/research/single_utterances/test'
 traindir = '/Users/karan/research/single_utterances/train'
+
+testdir = '/home/andrew/Dropbox (MIT)/6867_Project/single_utterances/test' #Andrew
+traindir = '/home/andrew/Dropbox (MIT)/6867_Project/single_utterances/train' #Andrew
+
 
 IMAGE_HEIGHT = 100
 IMAGE_WIDTH = 23
@@ -25,6 +30,12 @@ class SpecConvNet:
         self.load_dataset(traindir,testdir)
         self.graph = tf.Graph()
         self.define_tensorflow_graph()
+
+        #for plotting
+        self.batch_plot = []
+        self.val_plot = []
+        self.train_plot = []
+        self.steps = []
 
     def extract_data(self,traindir, testdir):
         print 'Loading datasets ... \n'
@@ -73,7 +84,8 @@ class SpecConvNet:
         layer2_num_hidden = 1024
         layer3_num_hidden = 1024
 
-        num_training_steps = 1501
+        num_training_steps = 1501 #1501
+        print 'doing', num_training_steps, 'training steps'
 
         # Add max pooling
         pooling = True
@@ -87,17 +99,17 @@ class SpecConvNet:
         weight_penalty = 0.0 # set to > 0.0 to apply weight penalty, 0.0 to remove
 
         with self.graph.as_default():
-        	# Input data
+            # Input data
             tf_train_batch = tf.placeholder(tf.float32, shape=(batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS))
             tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, NUM_LABELS))
             tf_valid_dataset = tf.constant(self.val_X)
             tf_test_dataset = tf.placeholder(tf.float32, shape=[len(self.val_X), IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS])
             tf_train_dataset = tf.placeholder(tf.float32, shape=[len(self.train_X), IMAGE_HEIGHT, IMAGE_WIDTH, NUM_CHANNELS])
 
-        	# Implement dropout
+            # Implement dropout
             dropout_keep_prob = tf.placeholder(tf.float32)
 
-        	# Network weights/parameters that will be learned
+            # Network weights/parameters that will be learned
             layer1_weights = tf.Variable(tf.truncated_normal([layer1_filter_height, layer1_filter_width, NUM_CHANNELS, layer1_depth], stddev=0.1))
             layer1_biases = tf.Variable(tf.zeros([layer1_depth]))
             layer1_feat_map_vert = int(math.ceil(float(IMAGE_HEIGHT) / layer1_stride))
@@ -107,38 +119,38 @@ class SpecConvNet:
                 layer1_feat_map_vert = int(math.ceil(float(layer1_feat_map_vert) / layer1_pool_stride_vert))
                 layer1_feat_map_hor = int(math.ceil(float(layer1_feat_map_hor) / layer1_pool_stride_hor))
 
-        	layer2_weights = tf.Variable(tf.truncated_normal(
-        		[layer1_feat_map_vert * layer1_feat_map_hor * layer1_depth, layer2_num_hidden], stddev=0.1))
-        	layer2_biases = tf.Variable(tf.constant(1.0, shape=[layer2_num_hidden]))
+            layer2_weights = tf.Variable(tf.truncated_normal(
+                [layer1_feat_map_vert * layer1_feat_map_hor * layer1_depth, layer2_num_hidden], stddev=0.1))
+            layer2_biases = tf.Variable(tf.constant(1.0, shape=[layer2_num_hidden]))
 
-        	layer3_weights = tf.Variable(tf.truncated_normal(
-        	  [layer3_num_hidden, NUM_LABELS], stddev=0.1))
-        	layer3_biases = tf.Variable(tf.constant(1.0, shape=[NUM_LABELS]))
+            layer3_weights = tf.Variable(tf.truncated_normal(
+              [layer3_num_hidden, NUM_LABELS], stddev=0.1))
+            layer3_biases = tf.Variable(tf.constant(1.0, shape=[NUM_LABELS]))
 
-			# Model
+            # Model
             def network_model(data):
-            	'''Define the actual network architecture'''
+                '''Define the actual network architecture'''
 
-            	# Layer 1
-            	conv1 = tf.nn.conv2d(data, layer1_weights, [1, layer1_stride, layer1_stride, 1], padding='SAME')
-            	hidden = tf.nn.relu(conv1 + layer1_biases)
+                # Layer 1
+                conv1 = tf.nn.conv2d(data, layer1_weights, [1, layer1_stride, layer1_stride, 1], padding='SAME')
+                hidden = tf.nn.relu(conv1 + layer1_biases)
 
-            	if pooling:
-            		hidden = tf.nn.max_pool(hidden, ksize=[1, layer1_pool_filter_height, layer1_pool_filter_width, 1],
-            						   strides=[1, layer1_pool_stride_vert, layer1_pool_stride_hor, 1],
-                         			   padding='SAME', name='pool1')
+                if pooling:
+                    hidden = tf.nn.max_pool(hidden, ksize=[1, layer1_pool_filter_height, layer1_pool_filter_width, 1],
+                                       strides=[1, layer1_pool_stride_vert, layer1_pool_stride_hor, 1],
+                                       padding='SAME', name='pool1')
 
-            	# Layer 2
-            	shape = hidden.get_shape().as_list()
-            	reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
-            	hidden = tf.nn.relu(tf.matmul(reshape, layer2_weights) + layer2_biases)
-            	hidden = tf.nn.dropout(hidden, dropout_keep_prob)
+                # Layer 2
+                shape = hidden.get_shape().as_list()
+                reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+                hidden = tf.nn.relu(tf.matmul(reshape, layer2_weights) + layer2_biases)
+                hidden = tf.nn.dropout(hidden, dropout_keep_prob)
 
-            	# Layer 3
-            	output = tf.matmul(hidden, layer3_weights) + layer3_biases
-            	return output
+                # Layer 3
+                output = tf.matmul(hidden, layer3_weights) + layer3_biases
+                return output
 
-			# Training computation
+            # Training computation
             logits = network_model(tf_train_batch)
             loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
 
@@ -155,44 +167,60 @@ class SpecConvNet:
             train_prediction = tf.nn.softmax(network_model(tf_train_dataset))
 
             def train_model(num_steps=num_training_steps):
-            	'''Train the model with minibatches in a tensorflow session'''
-            	with tf.Session(graph=self.graph) as session:
-            		tf.initialize_all_variables().run()
-            		print 'Initializing variables...'
+                '''Train the model with minibatches in a tensorflow session'''
+                with tf.Session(graph=self.graph) as session:
+                    tf.initialize_all_variables().run()
+                    print 'Initializing variables...'
 
-            		for step in range(num_steps):
-            			offset = (step * batch_size) % (self.train_Y.shape[0] - batch_size)
-            			batch_data = self.train_X[offset:(offset + batch_size), :, :, :]
-            			batch_labels = self.train_Y[offset:(offset + batch_size), :]
+                    for step in range(num_steps):
+                        offset = (step * batch_size) % (self.train_Y.shape[0] - batch_size)
+                        batch_data = self.train_X[offset:(offset + batch_size), :, :, :]
+                        batch_labels = self.train_Y[offset:(offset + batch_size), :]
 
-            			# Data to feed into the placeholder variables in the tensorflow graph
-            			feed_dict = {tf_train_batch : batch_data, tf_train_labels : batch_labels,
-            						 dropout_keep_prob: dropout_prob}
-            			_, l, predictions = session.run(
-            			  [optimizer, loss, batch_prediction], feed_dict=feed_dict)
-            			if (step % 100 == 0):
-            				train_preds = session.run(train_prediction, feed_dict={tf_train_dataset: self.train_X,
-            															   dropout_keep_prob : 1.0})
-            				val_preds = session.run(valid_prediction, feed_dict={dropout_keep_prob : 1.0})
-            				print ''
-            				print('Batch loss at step %d: %f' % (step, l))
-            				print('Batch training accuracy: %.1f%%' % accuracy(predictions, batch_labels))
-            				print('Validation accuracy: %.1f%%' % accuracy(val_preds, self.val_Y))
-            				print('Full train accuracy: %.1f%%' % accuracy(train_preds, self.train_Y))
+                        # Data to feed into the placeholder variables in the tensorflow graph
+                        feed_dict = {tf_train_batch : batch_data, tf_train_labels : batch_labels,
+                                     dropout_keep_prob: dropout_prob}
+                        _, l, predictions = session.run(
+                          [optimizer, loss, batch_prediction], feed_dict=feed_dict)
+                        if (step % 100 == 0):
+                            train_preds = session.run(train_prediction, feed_dict={tf_train_dataset: self.train_X,
+                                                                           dropout_keep_prob : 1.0})
+                            val_preds = session.run(valid_prediction, feed_dict={dropout_keep_prob : 1.0})
+                            print ''
+                            batch_acc = accuracy(predictions, batch_labels)
+                            val_acc = accuracy(val_preds, self.val_Y)
+                            train_acc = accuracy(train_preds, self.train_Y)
 
-			# save train model function so it can be called later
+                            print('Batch loss at step %d: %f' % (step, l))
+                            print('Batch training accuracy: %.1f%%' % batch_acc)
+                            print('Validation accuracy: %.1f%%' % val_acc)
+                            print('Full train accuracy: %.1f%%' % train_acc)
+
+                            self.batch_plot.append(batch_acc)
+                            self.val_plot.append(val_acc)
+                            self.train_plot.append(train_acc)
+                            self.steps.append(step)
+
+            # save train model function so it can be called later
             self.train_model = train_model
 
 def weight_decay_penalty(weights, penalty):
-	return penalty * sum([tf.nn.l2_loss(w) for w in weights])
+    return penalty * sum([tf.nn.l2_loss(w) for w in weights])
 
 def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
           / predictions.shape[0])
 
 if __name__ == '__main__':
-	t1 = time.time()
-	conv_net = SpecConvNet()
-	conv_net.train_model()
-	t2 = time.time()
-	print "Finished training. Total time taken:", t2-t1
+    t1 = time.time()
+    conv_net = SpecConvNet()
+    conv_net.train_model()
+    t2 = time.time()
+    print "Finished training. Total time taken:", t2-t1
+
+    print "Plotting"
+    plt.plot(conv_net.steps,conv_net.batch_plot,'ro-',label='batch')
+    plt.plot(conv_net.steps,conv_net.val_plot,'go-',label='validation')
+    plt.plot(conv_net.steps,conv_net.train_plot,'ro-',label='train')
+    plt.legend()
+    plt.show()
